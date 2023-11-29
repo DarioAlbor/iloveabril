102d30832642f162ff5dce3e6c6de46c39dcae1d
Dario Albor <dalbor@dgarzon.com>
2023-11-29 17:12:19 -0300

    PDFViewer Push

---
+++ src/components/PDFViewer/data/PDFListData.ts
@@ -0,0 +1,12 @@
+import IPDFList from '../../../components/PDFViewer/models/PDFList.model'
+
+export const Data: IPDFList[] = [
+  {
+    name: "Conocé nuestras ofertas",
+    title: "VIA SALUD",
+    description: "Description 1",
+    url: "pdf.pdf",
+    path: "path"
+  },
+
+]
